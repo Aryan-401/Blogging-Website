7b3a49dfe881791d0840b4da6c7e06a9cf96a639
Aryan-401 <aryan.raj.garg@gmail.com>
2023-04-17 20:11:39 +0530

    Upload to GitHub

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
+
 # Create your views here.
 
 def starting_page(request):
@@ -19,5 +20,5 @@ def post_detail(request, slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post,
-        "post_tags" : identified_post.tags.all()
+        "post_tags": identified_post.tags.all()
     })
